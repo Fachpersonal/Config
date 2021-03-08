f3ed6a457cef40e948bb7d1c31b1e2ac74f9ee3a
falscherIdiot <falscherIdiot@gmail.com>
2021-03-08 11:45:37 +0100

    Created basic Project

---
+++ Config/src/de/MissingNameException/Config.java
@@ -0,0 +1,14 @@
+package de.MissingNameException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Config {
+	
+	public Config(String path) throws IOException {
+		File f = new File(path);
+		
+		
+	}
+	
+}
